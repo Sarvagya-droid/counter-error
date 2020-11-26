aff1a4a487fef1df8e9bf8f7e908eca1dba079fb
Sarvagya-droid <66715764+Sarvagya-droid@users.noreply.github.com>
2020-11-26 12:37:00 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,42 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var counter = 3
+    var title = createElement('h1');
+    title.html("Car Racing Game");
+    title.position(displayWidth/2-100, 0);
+
+    this.input.position(displayWidth/2-40, displayHeight/2-80);
+    this.button.position(displayWidth/2+30, displayHeight/2);
+
+    if(this.button.mousePressed()){
+      counter = 1;
+      console.log("hello");
+    }
+    
+
+    if(counter === 1){
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2-70, displayHeight/4);
+    };
+
+  }
+}
